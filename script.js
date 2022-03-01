9e420e0951a3b2c0006ab24fd3c4b4986cb01295
Mac Barron <98175389+mac-mac-mac@users.noreply.github.com>
2022-03-01 03:16:27 +0000

    Added Lab 6 page, changes some styling

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-//runs when page loads
+//From Professor Fleenor's Examples
+//Runs when page loads
 window.onload = init;
-		function init() {
-			addLastUpdate();
-		}
+function init() {
+  addLastUpdate();
+}
 
+//From Professor Fleenor's Examples
 //Updates the HTML with the lastModified property of document
-//We are using it in the footer of each page except the index
 //We call it above so it runs when the page loads (window.onload)
 function addLastUpdate() {
   var modDate = document.lastModified;
   document.getElementById("update_date").innerHTML = modDate;
+}
+
+function makeArticleBlue() {
+  document.getElementById("article").style.backgroundColor = "#05B2DC";
+  document.getElementById("article").style.borderColor = "#004385";
+}
+
+function makeArticleOrange() {
+  document.getElementById("article").style.backgroundColor = "#D89D6A";
+  document.getElementById("article").style.borderColor = "#BB0A21";
+}
+
+function makeArticleGreen() {
+  document.getElementById("article").style.backgroundColor = "#C7D59F";
+  document.getElementById("article").style.borderColor = "#8FB339";
 }
